2dfc8cc11d68477e8e84bb2cd6704bb5d09ddb19
kotaro <turingsaketi@gmail.com>
2018-11-29 17:09:52 +0900

    added a lot of file

--- mysite/photo/views.py
+++ mysite/photo/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'photo/index.html')
+    return render(request, 'photo/index-image-slider.html')
